e7e9bc69f9691ccc0a4d20a45341cb4421762dbd
xXHighTideXx <84487860+TheHighTide@users.noreply.github.com>
2023-04-04 09:21:23 -0700

    Completed the most resent version
    This version (revision) is now completed because it has a fully working script. More being added soon

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ while isInApp == True:
         print("exit = Shuts down the app")
         print("random = Do some random things")
     elif userInput == "exit":
-        isInApp = false
+        isInApp = False
         print("Exiting app...")
     elif userInput == "random":
         print("What thing to you want to do random things with?\n1. art")
+        userInput = input()
         if userInput == "art":
             print("This will soon be art!")
         else:
