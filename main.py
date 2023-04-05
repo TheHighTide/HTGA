6e40f2cc08db3255dc630fbc85c55bbbf8663fb9
xXHighTideXx <84487860+TheHighTide@users.noreply.github.com>
2023-04-05 08:56:48 -0700

    Added a bunch of new things
    With this update, I added a bunch of new things to the code including a new generator. The new generator generates words, It is awesome!

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import random
 
 userInput = 'await'
 isInApp = True
+
+print("Welcome to HighTide's app! This is how you can check up on me!")
+print("What do you want to do?\nType 'help' for a list of options...")
 while isInApp == True:
-    print("Welcome to HighTide's app! This is how you can check up on me!")
-    print("What do you want to do?\nType 'help' for a list of options...")
     userInput = input()
     if userInput == "help":
         print("exit = Shuts down the app")
@@ -15,13 +16,18 @@ while isInApp == True:
         isInApp = False
         print("Exiting app...")
     elif userInput == "random":
-        print("What thing to you want to do random things with?\n1. art")
+        print("What thing to you want to do random things with?\n1. color\n2. word")
         userInput = input()
-        if userInput == "art":
-            with open("colors.txt", "r") as file:
+        if userInput == "color":
+            with open("Resources/colors.txt", "r") as file:
                 allText = file.read()
                 words = list(map(str, allText.split()))
                 draw_square(random.choice(words))
+        elif userInput == "word":
+            with open("Resources/word.txt", "r") as file:
+                allText = file.read()
+                words = list(map(str, allText.split()))
+                print(random.choice(words))
         else:
             print("Invalid option! Please try the command again!")
     else:
