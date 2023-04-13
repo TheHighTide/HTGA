ab6b4d4596e13e384fd8418bfad2c59dd42a3df6
xXHighTideXx <84487860+TheHighTide@users.noreply.github.com>
2023-04-13 13:53:45 -0700

    Added a new number generator to the main script
    This update is a fairly small update as it only adds a new generator type...

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+from random import randint
+from colorama import Fore
 from draw import *
 from fun import have_fun
 
@@ -15,7 +17,7 @@ while isInApp:
     elif userInput == "exit":
         have_fun("exit")
     elif userInput == "random":
-        print("What thing to you want to do random things with?\n1. color\n2. word")
+        print("What thing to you want to do random things with?\n1. color\n2. word\n3. number")
         userInput = input()
         if userInput == "color":
             with open("Resources/colors.txt", "r") as file:
@@ -29,7 +31,30 @@ while isInApp:
                 words = list(map(str, allText.split()))
                 randomWord = random.choice(words)
                 print(randomWord)
+        elif userInput == "number":
+            print("What type of number do you want to generate?\ntiny = 1 to 10\nsmall = 1 to 50\nmedium = 1 to 100\nlarge = 1 to 1000\nbig = 1 to 5000\nhuge = 1 to 10000")
+            userInput = input()
+            if userInput == "tiny":
+                randomNumber = randint(1, 10)
+                print("Your random number is: ", randomNumber)
+            elif userInput == "small":
+                randomNumber = randint(1, 50)
+                print("Your random number is: ", randomNumber)
+            elif userInput == "medium":
+                randomNumber = randint(1, 100)
+                print("Your random number is: ", randomNumber)
+            elif userInput == "large":
+                randomNumber = randint(1, 1000)
+                print("Your random number is: ", randomNumber)
+            elif userInput == "big":
+                randomNumber = randint(1, 5000)
+                print("Your random number is: ", randomNumber)
+            elif userInput == "huge":
+                randomNumber = randint(1, 10000)
+                print("Your random number is: ", randomNumber)
+            else:
+                print(Fore.RED, "Invalid Option! Please use one from a list of available options...", Fore.WHITE)
         else:
-            print("Invalid option! Please try the command again!")
+            print(Fore.RED,"Invalid option! Please try the command again!", Fore.WHITE)
     else:
-        print("Invalid command!\nPlease type 'help' for a list of commands!")
+        print(Fore.RED,"Invalid command!\nPlease type 'help' for a list of commands!",Fore.WHITE)
